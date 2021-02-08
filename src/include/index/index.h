b1b1d6bedcbce581f683d1a853ebd60b04de98af
atom <642600090@qq.com>
2021-02-08 21:35:40 +0800

    gtest

---
+++ src/include/index/index.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "disk/disk_manager.h"
+
+namespace mini_db{
+
+class index{
+    private:
+        
+};
+
+}
---
+++ src/test/disk/disk_manager_test.cpp
@@ -0,0 +1,17 @@
+#include "disk/disk_manager.h"
+#include <gtest/gtest.h>
+
+TEST(BufferPool,BasicTest){
+   auto disk_manager_ptr = new mini_db::DiskManager("test.db");
+   mini_db::page_id_t p_id = disk_manager_ptr->AllocatePage();
+
+
+   char hello_str[mini_db::PAGE_SIZE] = "hello new page";
+   disk_manager_ptr->WritePage(p_id,hello_str);
+   char buffer[mini_db::PAGE_SIZE];
+   disk_manager_ptr->ReadPage(p_id,buffer);
+   ASSERT_EQ(*buffer,*hello_str);
+
+
+   delete disk_manager_ptr;
+}
