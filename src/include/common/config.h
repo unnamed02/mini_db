20ef82ff1bb2dad57499a15c920e61aa7feac8cd
atom <642600090@qq.com>
2021-02-10 19:42:59 +0800

    config.h

--- src/include/common/config.h
+++ src/include/common/config.h
@@ -1,21 +1,4 @@
-//===----------------------------------------------------------------------===//
-//
-//                         BusTub
-//
-// config.h
-//
-// Identification: src/include/common/config.h
-//
-// Copyright (c) 2015-2019, Carnegie Mellon University Database Group
-//
-//===----------------------------------------------------------------------===//
-
 #pragma once
-
-#include <atomic>
-#include <chrono>  // NOLINT
-#include <cstdint>
-
 namespace mini_db {
 
 static constexpr int PAGE_SIZE = 4096;  
