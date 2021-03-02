4a397dd889b6980c04f9e08591a5873fc7791158
atom <642600090@qq.com>
2021-03-02 20:38:05 +0800

    add miss find test

--- test/page/page_test.cpp
+++ test/page/page_test.cpp
@@ -40,6 +40,32 @@ TEST(PAGE_TEST,BASIC_TEST){
     delete disk_manager_ptr;
 }
 
+TEST(PAGE_TEST,MISS_FIND_TEST){
+    auto disk_manager_ptr = new mini_db::DiskManager("test.db");
+    mini_db::page_id_t page_id = disk_manager_ptr->AllocatePage();
+    
+    char buffer[mini_db::PAGE_SIZE];
+    auto page_ptr = reinterpret_cast<mini_db::Page*>(buffer);
+    page_ptr->Init(0,page_id);
+    
+    char hello[6] = "hello";
+    for(int i = 0;i < 100;i++){
+        page_ptr->Append(12,hello);
+    }
+
+    disk_manager_ptr->WritePage(page_ptr->GetPageId(),buffer);
+
+    memset(buffer,0,mini_db::PAGE_SIZE);
+
+    disk_manager_ptr->ReadPage(page_id,buffer);
+    page_ptr = reinterpret_cast<mini_db::Page*>(buffer);
+
+    mini_db::page_offset_t start;
+    mini_db::page_offset_t length;
+    bool res = page_ptr->Find(1201,&start,&length);
+    ASSERT_EQ(res,false);
+}
+
 TEST(PAGE_TEST,MAX_INSERT_TEST){
     auto disk_manager_ptr = new mini_db::DiskManager("test.db");
     mini_db::page_id_t page_id = disk_manager_ptr->AllocatePage();
