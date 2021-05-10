a0048611d9db4d95abb1d3ba141330f2f1daedcf
atom <642600090@qq.com>
2021-05-10 20:40:02 +0800

    b+ tree : insert_into_parent

--- src/index/b_plus_tree_index.cpp
+++ src/index/b_plus_tree_index.cpp
@@ -24,15 +24,40 @@ page_id_t BPlusTreeIndex::WriteSlice(const duration_t duration,char* const conte
 
     old_page->MoveHalfTo(cur_page);
     disk_manager_ptr_->WritePage(old_page->GetPageId(),temp_frame.GetData());
-    cur_page->SetParentPageId(InsertIntoParent(cur_page->GetStart(),new_page_id));
+    cur_page->SetParentPageId(InsertIntoParent(old_page->GetParentPageId(),cur_page->GetStart(),new_page_id));
 
     return cur_page_id_;
+}
+
+page_id_t BPlusTreeIndex::InsertIntoParent(const page_id_t parent_page_id,const duration_t duration,const page_id_t child_page_id){
+    Frame parent_frame;
+    disk_manager_ptr_->ReadPage(parent_page_id,parent_frame->GetData());
+    auto parent_page_ptr = reinterpret_cast<Page*>(parent_frame->GetData());
+
+    if(parent_page_ptr->Append(duration,child_page_id)){
+        return parent_page_ptr->GetPageId();
+    }
+    
+    if(!parent_page_ptr->IsRootPage()){
+        Frame new_frame;
+        page_id_t new_page_id = AllocNewPage(false,false);
+        auto new_page_ptr = reinterpret_cast<Page*>(new_frame->GetData());
+        parent_page_ptr->MoveHalfTo(new_page_ptr);
+
+        //TODO: could be buggy here,since InsertIntoParent may read this page again
+        disk_manager_ptr_->WritePage(parent_page_id,parent_page_ptr->GetDat());
+        new_page_ptr->SetParentPageId(InsertIntoParent(parent_page_id->GetParentPageId(),new_page_ptr->GetStart(),new_page_id));
+        disk_manager_ptr_->WritePage(new_page_id,new_frame->GetData());
+        return new_page_id;
+    }else{
+        
+    }
+
 }
 
 page_id_t BPlusTreeIndex::GetPage(const duration_t duration){
 
 }
-
 
 BPlusTreeIndex::BPlusTreeIndex(DiskManager* disk_manager,time_scale_t scale,uint32_t max_pages,uint32_t buffer_size):
 Index(disk_manager,scale,max_pages,buffer_size,false,true){
@@ -47,23 +72,6 @@ Index(disk_manager,scale,max_pages,buffer_size,false,true){
 
 }
 
-page_id_t InsertIntoParent(const page_id_t parent_page_id,const duration_t duration,const page_id_t child_page_id){
-    Frame parent_frame;
-    disk_manager_ptr_->ReadPage(parent_page_id,parent_frame->GetData());
-    auto parent_page_ptr = reinterpret_cast<Page*>(parent_frame->GetData());
-
-    if(parent_page_ptr->Append(duration,child_page_id)){
-        return parent_page_ptr->GetPageId();
-    }
-    
-    if(!parent_page_ptr->IsRootPage()){
-        Frame new_frame;
-        page_id_t new_page_id = AllocNewPage(false,false);
-        auto new_page_ptr = reinterpret_cast<Page*>(new_frame->GetData());
-        parent_page_ptr->MoveHalfTo(new_page_ptr);
-    }
-
-}
 
 BPlusTreeIndex::~BPlusTreeIndex(){
 
